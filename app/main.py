20b4d2a6ce472bf98264c6f0b95fda2833c39295
vegardbeider <vegard@sgjerde.net>
2023-10-28 12:57:31 +0200

    remove non-functional metadata creator

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 import requests
 from datetime import date
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, TIT2, TPE1, TALB, TRCK, TDRC, TCON
 
 def getDate():
     d = date.today().__str__()
@@ -10,28 +8,6 @@ def getDate():
 def writeFile(path, fileName, source):
     with open(path+fileName, "wb") as f:
         f.write(source.content)
-
-def setMetaData(path, fileName):
-    # create a new ID3 tag object or load one from the file if it exists
-    tags = ID3(path+fileName)
-
-    # set the title
-    tags.add(TIT2(encoding=3, text=fileName))
-
-    # set the artist
-    tags.add(TPE1(encoding=3, text='bibel.no'))
-
-    # set the album
-    tags.add(TALB(encoding=3, text='Verse of the Day'))
-
-    # set the year
-    tags.add(TDRC(encoding=3, text=fileName[4]+fileName[5]))
-
-    # set the genre
-    tags.add(TCON(encoding=3, text='Bible Verse'))
-
-    # save the tags back to the file
-    tags.save(path+fileName)
 
 base = "https://bibelselskapet-web.s3.eu-north-1.amazonaws.com/audios/uploads/"
 day = getDate()
